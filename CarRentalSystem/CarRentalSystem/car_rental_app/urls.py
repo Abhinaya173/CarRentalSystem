f3b8a2bb6e4871e044c3be34359d46210efa8307
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2023-10-17 19:35:40 +0700

    Update urls.py

--- CarRentalSystem/CarRentalSystem/car_rental_app/urls.py
+++ CarRentalSystem/CarRentalSystem/car_rental_app/urls.py
@@ -19,11 +19,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from system.views import admin_car_list, admin_msg, order_list, car_created, order_update, order_delete, msg_delete
-from accounts.views import (login_view, register_view, logout_view, registerPage, loginPage)
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', admin_car_list, name='adminIndex'),
+    path('admin/',admin.site.urls),
+    path('', admin_car_list, name='admin_index'),
     path('listOrder/', order_list, name = "order_list"),
     re_path(r'^(?P<id>\d+)/editOrder/$', order_update, name = "order_edit"),
     re_path(r'^(?P<id>\d+)/deleteOrder/$', order_delete, name = "order_delete"),
@@ -31,10 +31,11 @@ urlpatterns = [
     path('message/', admin_msg, name='message'),
     re_path(r'^(?P<id>\d+)/deletemsg/$', msg_delete, name = "msg_delete"),
     path('car/', include('system.urls')),
-    path('', loginPage, name='login'),
     path('logout/', logout_view, name='logout'),
-    # path('register/', register_view, name='register'),
-    path('register/', registerPage, name= "register"),
+    path('register/', register_view, name='register'),
+    path('accounts/', include ('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
